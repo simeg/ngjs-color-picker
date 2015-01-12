3f0203a43e0aac6edfb311762b6b15e607c4a856
Simon Egersand <s.egersand@gmail.com>
2015-01-12 21:37:41 +0100

    Possible float left bug fix

--- js/ngjs-color-picker.js
+++ js/ngjs-color-picker.js
@@ -72,7 +72,6 @@ angular.module('ngjsColorPicker', [])
           scope.css = {};
           // Set bar to horizontal/vertical
           scope.css.display = (scope.options.horizontal ? 'inline-block' : 'block');
-          scope.css['float'] = "left";
           // Set size of squares
           scope.css.width = scope.css.height = scope.options.size + 'px';
 
@@ -85,7 +84,7 @@ angular.module('ngjsColorPicker', [])
               //element.children().css('height', scope.options.size*(scope.colors.length/scope.options.columns));
               element.find('ul').css('width', scope.options.columns*(parseInt(scope.css.width.substr(0,indexOfPx))));
               element.find('ul').css('height', scope.options.size*(scope.colors.length/scope.options.columns));
-              scope.css.float = 'left';
+              scope.css.cssFloat = 'left';
           }
 
           // Set if rounded corners (horizontal or vertical)
