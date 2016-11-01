955bdcded334a5a5c107efd8b57f90e8b9985e7f
Simon Egersand <s.egersand@gmail.com>
2016-11-01 21:33:04 +0100

    Add more examples to dev server

--- dev/src/app/app.js
+++ dev/src/app/app.js
@@ -2,36 +2,48 @@ import angular from 'angular';
 require('./ngjs-color-picker');
 
 class AppCtrl {
-  constructor() {
+    constructor() {
 
-    // Set $scope variables here on _this_
-    this.options = {
-      // randomColors: 12,
-      // size: 30,
-      // columns: 4,
-      // roundCorners: true,
-    };
+        // Set $scope variables here on _this_
+        this.customOptions = {
+            size: 30,
+            roundCorners: true
+        };
 
-    /*this.optionsGradient = {
-      start: '#BA693E',
-      count: 10,
-      step: 1
-    };*/
-  }
+        this.optionsRandom = {
+            randomColors: 10
+        };
+
+        this.optionsGradient = {
+            start: '#BA693E',
+            count: 10,
+            step: 1
+        };
+
+        this.optionsVertical = {
+            horizontal: false,
+            total: 5
+        };
+
+        this.optionsColumn = {
+            columns: 4,
+            roundCorners: true
+        };
+    }
 }
 
 /* Angular configuration (no need to touch this) */
 let app = () => {
-  return {
-    template: require('./app.html'),
-    controller: 'AppCtrl',
-    controllerAs: 'app'
-  }
+    return {
+        template: require('./app.html'),
+        controller: 'AppCtrl',
+        controllerAs: 'app'
+    }
 };
 
 const MODULE_NAME = 'app';
 angular.module(MODULE_NAME, ['ngjsColorPicker'])
-  .directive('app', app)
-  .controller('AppCtrl', AppCtrl);
+    .directive('app', app)
+    .controller('AppCtrl', AppCtrl);
 
 export default MODULE_NAME;
