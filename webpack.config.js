f0813499757a99bd30fe44c080ef3406d2032115
Simon Egersand <s.egersand@gmail.com>
2017-07-30 13:17:35 +0200

    Fix webpack version bump config

--- webpack.config.js
+++ webpack.config.js
@@ -73,14 +73,13 @@ module.exports = function makeWebpackConfig () {
 
     // Initialize module
     config.module = {
-        preLoaders: [],
         loaders: [{
             // JS LOADER
             // Reference: https://github.com/babel/babel-loader
             // Transpile .js files using babel-loader
             // Compiles ES6 and ES7 into ES5 code
             test: /\.js$/,
-            loader: 'babel',
+            loader: 'babel-loader',
             exclude: /node_modules/
         }, {
             // ASSET LOADER
@@ -96,7 +95,7 @@ module.exports = function makeWebpackConfig () {
             // Reference: https://github.com/webpack/raw-loader
             // Allow loading html through js
             test: /\.html$/,
-            loader: 'raw'
+            loader: 'raw-loader'
         }]
     };
 
